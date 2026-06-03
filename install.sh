b7be0f749b698deb3b5f46b96872ed9acf1409ce
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2026-06-03 10:19:27 +0100

    Prompt before overwriting existing file

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ mkdir -p "$HOME/.config"
 mkdir -p "$HOME/.config/devenv"
 
 # Symlink folders
-#ln -sn "$HOME/code/dotfiles/.config/nvim" "$HOME/.config/nvim"
-ln -sn "$HOME/code/dotfiles/.config/devenv" "$HOME/.config/devenv"
+#ln -sni "$HOME/code/dotfiles/.config/nvim" "$HOME/.config/nvim"
+ln -sni "$HOME/code/dotfiles/.config/devenv" "$HOME/.config/devenv"
 
 # Symlink root-level files
-ln -s "$HOME/code/dotfiles/shell/.zshrc" "$HOME/.zshrc"
-ln -s "$HOME/code/dotfiles/git/.gitconfig" "$HOME/.gitconfig"
-ln -s "$HOME/code/dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
+ln -si "$HOME/code/dotfiles/shell/.zshrc" "$HOME/.zshrc"
+ln -si "$HOME/code/dotfiles/git/.gitconfig" "$HOME/.gitconfig"
+ln -si "$HOME/code/dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
 
 echo "Dotfiles symlinked successfully!"
