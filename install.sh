57e180c9669d6892e0e58bc42c9a5de1719afcb1
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2026-06-04 08:58:07 +0100

    Update config

--- install.sh
+++ install.sh
@@ -5,12 +5,16 @@ mkdir -p "$HOME/.config"
 mkdir -p "$HOME/.config/devenv"
 
 # Symlink folders
-#ln -sni "$HOME/code/dotfiles/.config/nvim" "$HOME/.config/nvim"
-ln -sni "$HOME/code/dotfiles/.config/devenv" "$HOME/.config/devenv"
+#ln -sni "$HOME/code/dotfiles/.config/nvim" "$HOME/.config"
+ln -sni "$HOME/code/dotfiles/.config/mise" "$HOME/.config"
+ln -sni "$HOME/code/dotfiles/.config/devenv" "$HOME/.config"
 
 # Symlink root-level files
 ln -si "$HOME/code/dotfiles/shell/.zshrc" "$HOME/.zshrc"
 ln -si "$HOME/code/dotfiles/git/.gitconfig" "$HOME/.gitconfig"
 ln -si "$HOME/code/dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
+
+chsh -s "$(which zsh)"
+source "$HOME/.zshrc"
 
 echo "Dotfiles symlinked successfully!"
