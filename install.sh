638769e9d75904aa906715f22f8002cb7b673d6b
Kelvin Chappell <1722550+kelvin-chappell@users.noreply.github.com>
2026-06-04 09:09:09 +0100

    Just keep lines for use in dev container

--- install.sh
+++ install.sh
@@ -6,15 +6,15 @@ mkdir -p "$HOME/.config/devenv"
 
 # Symlink folders
 #ln -sni "$HOME/code/dotfiles/.config/nvim" "$HOME/.config"
-ln -sni "$HOME/code/dotfiles/.config/mise" "$HOME/.config"
-ln -sni "$HOME/code/dotfiles/.config/devenv" "$HOME/.config"
+#ln -sni "$HOME/code/dotfiles/.config/mise" "$HOME/.config"
+#ln -sni "$HOME/code/dotfiles/.config/devenv" "$HOME/.config"
 
 # Symlink root-level files
-ln -si "$HOME/code/dotfiles/shell/.zshrc" "$HOME/.zshrc"
+#ln -si "$HOME/code/dotfiles/shell/.zshrc" "$HOME/.zshrc"
 ln -si "$HOME/code/dotfiles/git/.gitconfig" "$HOME/.gitconfig"
 ln -si "$HOME/code/dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
 
-chsh -s "$(which zsh)"
-source "$HOME/.zshrc"
+#chsh -s "$(which zsh)"
+#source "$HOME/.zshrc"
 
 echo "Dotfiles symlinked successfully!"
